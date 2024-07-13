a06e2a2be091fc9f1fe734d0099da9c0e76b06f9
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-13 12:14:13 +0200

    Bearbeitung von Abrufaufträgen hinzugefügt.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
+import { setupStore } from './store/store.ts'
+
+const store = setupStore()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
