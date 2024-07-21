31cf79ca4123f24893cdfece88b91cc8ba36ffec
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-21 17:33:25 +0200

    Beispiele zum Erstellen, Aktualisieren und Löschen mit Rtk Query hinzugefügt.

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,17 +1,36 @@
-
 import {postAPI} from "../services/PostService";
 import PostItem from "./PostItem";
-
+import {IPost} from "../models/IPost";
+import { useState } from "react";
 
 const PostContainer = () => {
-    
-    const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllPostsQuery(5)
+    const [limit, setLimit] = useState(100);
+    const {data: posts, error, isLoading, refetch} =  postAPI.useFetchAllPostsQuery(limit)
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [updatePost, {error:uerror}] = postAPI.useUpdatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
+
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
 
     return (
         <div>
             <div className="post__list">
+                <button onClick={handleCreate}>Add new post</button>
+                {isLoading && <h1>Lädt...</h1>}
+                {error && <h1>Fehler beim Laden</h1>}
                 {posts && posts.map(post =>
-                    <PostItem key={post.id} post={post}/>
+                    <PostItem remove={handleRemove} update={handleUpdate} key={post.id} post={post}/>
                 )}
             </div>
         </div>
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -4,16 +4,20 @@ import {IPost} from "../models/IPost";
 
 interface PostItemProps {
     post: IPost;
+    remove: (post: IPost) => void;
+    update: (post: IPost) => void;
 }
 
-const PostItem: FC<PostItemProps> = ({post, }) => {
+const PostItem: FC<PostItemProps> = ({post, remove, update}) => {
 
     const handleRemove = (event: React.MouseEvent) => {
-
+        event.stopPropagation()
+        remove(post)
     }
 
     const handleUpdate = (event: React.MouseEvent) => {
-  
+        const title = prompt() || ""
+        update({...post, title})
     }
 
     return (
